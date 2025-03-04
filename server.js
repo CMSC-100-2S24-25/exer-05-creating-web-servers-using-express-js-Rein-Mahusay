838e71673dfdfff25a42c57c89f897db0530c668
Rein-Mahusay <rbmahusay@up.edu.ph>
2025-03-04 21:02:34 +0800

    Tried first with the post method, not doing anything yet.

--- server.js
+++ server.js
@@ -5,13 +5,32 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//sample from handout
-app.get('/', (req, res) => {
-    res.send('Hello!');
+//start with the post method for adding books to the text file
+app.post('/add-book', (req, res) => {
+    //req.body = parser for reading the body of incoming requests
+    //from the handout
+    const { bkName, isbn, author, yearPub } = req.body;
+
+    if (!bkName && !isbn && !author && !yearPub) {
+        return res.send('All fields must be a non-empty string.');
+    }
+
+    //simple format for the book information added
+    //bookname,isbn,author,yearpub
+    //like in the past exer, but shorter para straightforward (mismong laman na)
+    const data = '${bkName},${isbn},${author},${yearPub}\n';
+
+    //file operations here
+
 });
 
-app.listen(3000, () => { console.log('Server started at port 3000.') });
+// //sample from handout
+// app.get('/', (req, res) => {
+//     res.send('Hello!');
+// });
 
-app.post('/submit-data', (req, res) => {
-    res.send('Received a POST request.');
-});
+// app.listen(3000, () => { console.log('Server started at port 3000.') });
+
+// app.post('/submit-data', (req, res) => {
+//     res.send('Received a POST request.');
+// });
